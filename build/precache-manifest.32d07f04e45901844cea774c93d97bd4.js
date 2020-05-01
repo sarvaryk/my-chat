9d74eb9e62579e01c87a52bc02d320631fecad98
sarvaryk <sarvarykrisztian@gmail.com>
2020-05-01 15:14:43 +0200

    Uploading the build files

---
+++ build/precache-manifest.32d07f04e45901844cea774c93d97bd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca0c62881891efa2726e1530973ce386",
+    "url": "./index.html"
+  },
+  {
+    "revision": "41f5c4a76ff3c6f05ea5",
+    "url": "./static/css/main.2b5da8de.chunk.css"
+  },
+  {
+    "revision": "9c43dcac9fb25086c010",
+    "url": "./static/js/2.f9671187.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.f9671187.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41f5c4a76ff3c6f05ea5",
+    "url": "./static/js/main.b88a0586.chunk.js"
+  },
+  {
+    "revision": "13581df0e096de695991",
+    "url": "./static/js/runtime-main.3fe63c3a.js"
+  }
+]);
