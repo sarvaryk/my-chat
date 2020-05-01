c12944634961c4f9bada8e29cb6b91a5a94cce13
sarvaryk <sarvarykrisztian@gmail.com>
2020-05-01 15:09:07 +0200

    Commiting the modified and created files for the HW.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { Login } from './Login'
+import { proxy } from './Proxy';
+import { Main } from './Main';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = { loginSuccessful: false }
+
+  componentDidMount() {
+      proxy.addEventListener("login", () => this.setState({ loginSuccessful: true}), this);
+  }
+
+  render() {
+    return (
+      <div className="app">
+        { this.state.loginSuccessful ? <Main /> : <Login /> }
+      </div>
+    );
+  }
 }
-
-export default App;
